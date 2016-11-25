8af4f440165d61d1100321ae181e3912277c925c
yrkv <yegorushka.kuznetsov@gmail.com>
2016-11-24 22:35:39 -0600

    changed rendering method and added some stuff

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,33 +1,35 @@
 package game;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import game.graphics.Camera;
 import game.graphics.Screen;
 import game.level.Level;
 
-public class Game extends JComponent
-{
+public class Game extends Canvas implements Runnable {
 	public static int totalTicks = 0;
 	public static int width = 1024;
 	public static int height = width * 9 / 16; // 576
+	
+	private boolean running = false;
 
+	private Thread thread;
 	private JFrame frame;
 	private Screen screen;
 	private Level level;
 	private Camera cam;
 
-//	private BufferedImage image = new BufferedImage(width, height,
-//			BufferedImage.TYPE_INT_RGB);
-//	private int[] pixels = ((DataBufferInt) image.getRaster()
-//			.getDataBuffer()).getData();
+	private BufferedImage image = new BufferedImage(width, height,
+			BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster()
+			.getDataBuffer()).getData();
 
 	public Game()
 	{
@@ -55,20 +57,79 @@ public class Game extends JComponent
 		game.frame.add(game);
 
 		game.frame.setVisible(true);
-	}
-
-	public void paintComponent(Graphics g)
-	{
-		Graphics2D g2 = (Graphics2D) g;
 		
-		g2.setColor(Color.black);
+		game.start();
+	}
+	
+	private synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+		running = true;
+	}
+	
+	private synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void update() {
+		
+	}
+	
+	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+					update();
+					updates++;
+					totalTicks++;
+					delta--;
+			}
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle("game | " + updates + " ups, " + frames + " fps"); 
+				// ups is so shaky because of how high fps is, it wont be an issue once the game has more stuff.
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	private void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+//		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		// I'm going to have to swap to a system using this if I want to map images to the models.
+		// I'm still not sure if I want to do that.
+		
+		g.setColor(Color.black);
 		
 		int[][][] points = cam.view(3);
 
 		for (int i = 0; i < points.length; i++) {
 			int[] a = {points[i][0][0], points[i][1][0], points[i][2][0]};
 			int[] b = {points[i][0][1], points[i][1][1], points[i][2][1]};
-			g2.drawPolygon(a, b, 3);
+			g.drawPolygon(a, b, 3);
 		}
 		
 		points = cam.view(4);
@@ -76,9 +137,10 @@ public class Game extends JComponent
 		for (int i = 0; i < points.length; i++) {
 			int[] a = {points[i][0][0], points[i][1][0], points[i][2][0], points[i][3][0]};
 			int[] b = {points[i][0][1], points[i][1][1], points[i][2][1], points[i][3][1]};
-			g2.drawPolygon(a, b, 4);
+			g.drawPolygon(a, b, 4);
 		}
-
-		//g2.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		
+		g.dispose();
+		bs.show();
 	}
 }
